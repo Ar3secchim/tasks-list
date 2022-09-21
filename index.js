af3edaf0fcc8b9f028e97272918d10f1e2d37c4e
Renara Secchim <renarasecchim@gmail.com>
2022-09-21 18:03:31 -0300

    feat: function creation to add item in array

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-let input = document.querySelector("input")
+let Input = document.querySelector("#stringTask")
+let btn = document.querySelector("#btn")
 
 let tasksList = []
 
-function newTasks(string){
-  const task = {
-    text,
-    checked: false
+btn.onclick = function (){
+  let stringInput = Input.value
+
+  if(stringInput){
+    let task = {
+      name: stringInput,
+      checked: false
+    }
+    tasksList.push(task)
+    saveListTask(tasksList)
+  }else{
+    // colocar mensagem de erro na inferface
+    console.log("adiciona alguma tarefa")
   }
-  tasksList.push(task)
   console.log(tasksList)
 }
 
-let button = document.getElementById("button-submit")
+function saveListTask (tasksList){
+  for (let index = 0; index < tasksList.length; index++) {
+    const list = tasksList[index];
 
-button.addEventListener('submit', event =>{
+// correção para adicionar nas divs
+    const divList = document.createElement("div")
+    divList.classList.add("list-task")
+
+    const titleTask = document.createElement("h2")
+    titleTask.innerText = tasksList[index].name
+    divList.appendChild(titleTask)
+
+    const checkedTask = document.createElement('img')
+    checkedTask.src = "/assets/checked.svg"
+    divList.insertBefore(checkedTask, titleTask)
+
+    const removeTask = document.createElement("img")
+    removeTask.src = "/assets/remove.svg"
+    divList.appendChild(removeTask)
+
+    // divList.style.display
+    console.log(divList)
+  }
+}
+
+// prevenindo comportamento padrão do evento
+let form = document.querySelector('form')
+form.addEventListener('submit', event =>{
   event.preventDefault()
-  console.log('deu certo')
 })
